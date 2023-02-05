954f2ae0d9c8bfc3d0c13a6e39a738dd951874ec
brualvess <oliveirabrunna2210@gmail.com>
2023-02-05 09:29:02 -0300

    listar comandos

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from sqlite3 import IntegrityError
 import discord
-from discord.ext import  commands, tasks
+from discord.ext import commands
 from database import Database
 from dotenv import load_dotenv
 import os
@@ -14,16 +14,19 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 database = Database()
 
+
 @bot.event
 async def on_ready():
     await database.create_tables()
     print(f"Tudo ok! Estou conectado como {bot.user}")
+
 
 @bot.command(name="ping")
 async def ping(ctx):
     response = "pong"
 
     await ctx.send(response)
+
 
 @bot.command(name="inserir_categoria")
 async def insert_category(ctx, category):
@@ -34,6 +37,7 @@ async def insert_category(ctx, category):
     except IntegrityError:
         await ctx.send("categoria já existe!")
 
+
 @bot.command(name="listar_categorias")
 async def list_categories(ctx):
     categories = await database.list_category()
@@ -41,11 +45,13 @@ async def list_categories(ctx):
     response += "\n".join([category[0] for category in categories])
     await ctx.send(response)
 
+
 @bot.command(name="remover_categoria")
 async def remove_categories(ctx, category):
     await database.remove_category(category)
-    
+
     await ctx.send(f"categoria {category} removida")
+
 
 @bot.command(name="inserir_receita")
 async def insert_recipe(ctx, name, link, category):
@@ -57,7 +63,7 @@ async def insert_recipe(ctx, name, link, category):
         await ctx.send("categoria não encontrada")
     except IntegrityError:
         await ctx.send(f"receita já existe na categoria {category}")
-    
+
 
 @bot.command(name="listar_receitas")
 async def list_recipes_category(ctx, category):
@@ -65,6 +71,7 @@ async def list_recipes_category(ctx, category):
     response = "aqui estão todas as receitas:\n"
     response += "\n".join([recipe[0] for recipe in recipes])
     await ctx.send(response)
+
 
 @bot.command(name="buscar_receita")
 async def get_recipe(ctx, recipe, category):
@@ -75,11 +82,41 @@ async def get_recipe(ctx, recipe, category):
         response = f"aqui está a receita **{recipe_data[0][0]}**: {recipe_data[0][1]}"
         await ctx.send(response)
 
+
 @bot.command(name="remover_receita")
 async def remove_recipe(ctx, recipe, category):
     await database.remove_recipe(recipe, category)
 
     await ctx.send(f"receita {recipe} removida")
+
+
+@bot.command(name="listar_comandos")
+async def list_comands(ctx):
+    response = f'''
+        ** Comandos: **
+        *- criar nova categoria:*
+            ` !inserir_categoria $NOME_DA_CATEGORIA `
+
+        *- buscar todas categorias existentes:*
+            ` !listar_categorias `
+
+        *- excluir categoria:*
+            ` !remover_categoria $NOME_DA_CATEGORIA `
+
+        *- criar uma nova receita:*
+            ` !inserir_receita $NOME_DA_RECEITA $LINK_DA_RECEITA $NOME_DA_CATEGORIA `
+    
+          *- buscar receitas por categoria:*
+            ` !listar_receitas $NOME_DA_CATEGORIA `
+
+        *- buscar uma receita específica:*
+            ` !buscar_receita $NOME_DA_RECEITA $NOME_DA_CATEGORIA `
+
+        *- excluir receita:*
+            ` !remover_receita $NOME_DA_RECEITA $NOME_DA_CATEGORIA `
+
+    '''
+    await ctx.send(response)
 
 token = os.getenv("TOKEN")
 bot.run(token)
